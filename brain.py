2f9fe082f0063baf955b125364dfa3db4057e045
iclwy524 <patricklichun@gmail.com>
2016-12-20 00:44:26 +0800

    repair indent

--- brain.py
+++ brain.py
@@ -108,19 +108,19 @@ while 1:
 		break
 IOClass=obd.get_port()
 while 1:
-	print "sensor 12"
-	print IOClass.sensor(12)
-	print "sensor 13"
-	print IOClass.sensor(13)
+    print "sensor 12"
+    print IOClass.sensor(12)
+    print "sensor 13"
+    print IOClass.sensor(13)
     print "sensor of total results from caputure"
     print obd.allSensorData
 
-	try:
-		print "DTC"
-		print IOClass.get_dtc()
-	except:
-		print "print DTC error"
-	time.sleep(0.5)
+    try:
+        print "DTC"
+        print IOClass.get_dtc()
+    except:
+        print "print DTC error"
+        time.sleep(0.5)
 
 
 if obd.is_connected():
